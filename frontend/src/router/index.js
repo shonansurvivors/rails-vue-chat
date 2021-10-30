b1418fc4e557ff6a7dfbf2717cdeb0129d12a58d
shonansurvivors <shonansurvivors@gmail.com>
2021-10-30 22:42:13 +0900

    create chat room vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from "../views/Welcome";
+import ChatRoom from "../views/ChatRoom";
 
 const routes = [
   {
     path: '/',
     name: 'Welcome',
     component: Welcome
+  },
+  {
+    path: '/chatroom',
+    name: 'ChatRoom',
+    component: ChatRoom
   }
 ]
 

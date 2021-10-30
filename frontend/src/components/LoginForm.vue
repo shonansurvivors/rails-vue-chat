39d71fec9fc6fb7a9d5de5cfbbb81e0037cead8d
shonansurvivors <shonansurvivors@gmail.com>
2021-10-30 22:47:58 +0900

    redirect to chat room

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -12,6 +12,7 @@
 import axios from 'axios'
 
 export default {
+  emits: ['redirectToChatRoom'],
   data () {
     return {
       email: '',
@@ -30,6 +31,10 @@ export default {
         })
         if (!res) {
           throw new Error('メールアドレスかパスワードが違います')
+        }
+
+        if (!this.error) {
+          this.$emit('redirectToChatRoom')
         }
 
         console.log({res})
--- frontend/src/components/SignupForm.vue
+++ frontend/src/components/SignupForm.vue
@@ -14,6 +14,7 @@
 import axios from 'axios'
 
 export default {
+  emits: ['redirectToChatRoom'],
   data () {
     return {
       name: '',
@@ -36,6 +37,11 @@ export default {
         if (!res) {
           throw new Error('アカウントを登録できませんでした')
         }
+
+        if (!this.error) {
+          this.$emit('redirectToChatRoom')
+        }
+
         console.log({res})
         return res
       } catch (error) {

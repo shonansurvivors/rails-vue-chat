d77c8cdcede5b5730c3228f254d553f413822899
shonansurvivors <shonansurvivors@gmail.com>
2021-10-30 20:10:02 +0900

    switch signup and login

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -1,8 +1,14 @@
 <template>
   <div className="container welcome">
     <p>ようこそ！</p>
-    <LoginForm />
-    <SignupForm />
+    <div v-if="shouldShowLoginForm">
+      <LoginForm />
+      <p class="change-form">初めての方は<span @click="shouldShowLoginForm = false">こちら</span>をクリック</p>
+    </div>
+    <div v-if="!shouldShowLoginForm">
+      <SignupForm />
+      <p class="change-form">アカウントをお持ちの方は<span @click="shouldShowLoginForm = true">こちら</span>をクリック</p>
+    </div>
   </div>
 </template>
 
@@ -11,38 +17,48 @@ import LoginForm from '../components/LoginForm.vue'
 import SignupForm from "../components/SignupForm";
 export default {
   components: { LoginForm, SignupForm },
+  data () {
+    return {
+      shouldShowLoginForm: true
+    }
+  }
 }
+
 </script>
 
 <style>
-.welcome {
-  text-align: center;
-  padding: 20px 0;
-}
+  .welcome {
+    text-align: center;
+    padding: 20px 0;
+  }
 
-.welcome form {
-  width: 300px;
-  margin: 20px auto;
-}
-.welcome label {
-  display: block;
-  margin: 20px 0 10px;
-}
-.welcome input {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px auto;
-  border-radius: 4px;
-  border: 1px solid #eee;
-  outline: none;
-  box-sizing: border-box;
-}
-.welcome span{
-  font-weight: bold;
-  text-decoration: underline;
-  cursor: pointer;
-}
-.welcome button {
-  margin: 20px auto;
-}
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  .welcome input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px auto;
+    border-radius: 4px;
+    border: 1px solid #eee;
+    outline: none;
+    box-sizing: border-box;
+  }
+  .welcome span{
+    font-weight: bold;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  .welcome button {
+    margin: 20px auto;
+  }
+  .change-form {
+    font-size: 14px;
+    margin: 10px;
+  }
 </style>

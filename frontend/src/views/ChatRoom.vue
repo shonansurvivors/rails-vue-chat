698eff4380415ebfabfa6cabe0e011a7884ea30b
shonansurvivors <shonansurvivors@gmail.com>
2021-10-31 14:06:38 +0900

    display messages

--- frontend/src/views/ChatRoom.vue
+++ frontend/src/views/ChatRoom.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="container">
     <Navbar />
-    <ChatWindow />
+    <ChatWindow :messages="messages" />
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
 import ChatWindow from '../components/ChatWindow'
+import axios from "axios";
 
 export default {
-  components: {Navbar, ChatWindow}
+  components: {Navbar, ChatWindow},
+  data () {
+    return {
+      messages: [],
+    }
+  },
+  methods: {
+    async getMessages () {
+      try {
+        const res = await axios.get('http://localhost:3000/messages', {
+          headers: {
+            uid: window.localStorage.getItem('uid'),
+            "access-token": window.localStorage.getItem('access-token'),
+            client:window.localStorage.getItem('client')
+          }
+        })
+        if (!res) {
+          new Error('メッセージ一覧を取得できませんでした')
+        }
+        this.messages = res.data
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+  mounted() {
+    this.getMessages()
+  },
 }
 </script>
 
